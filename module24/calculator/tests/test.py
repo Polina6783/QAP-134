521b2f6ff8722cb18487d933d1f8adc83f163b2b
Polina6783 <lina6783@yandex.ru>
2023-10-17 00:34:50 +0300

    add hw 24.2.4

---
+++ module24/calculator/tests/test.py
@@ -0,0 +1,23 @@
+import pytest
+
+from module24.calculator.app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply(self):
+        assert self.calc.multiply(self, 5, 6) == 30
+
+    def test_division(self):
+        assert self.calc.division(self, 10, 2) == 5
+
+    def test_subtraction(self):
+        assert self.calc.subtraction(self, 8, 6) == 2
+
+    def test_adding(self):
+        assert self.calc.adding(self, 5, 7) == 12
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
